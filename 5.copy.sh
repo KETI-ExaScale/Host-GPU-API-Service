942fe07f41e7ee9468339709a4d16669d46c5705
juhyunkil <wngus0331@gmail.com>
2023-11-10 10:59:16 +0900

    copy

---
+++ 5.copy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+declare -A node_ip_mapping
+
+node_info=$(kubectl get nodes -o custom-columns="NAME:.metadata.name,INTERNAL-IP:.status.addresses[0].address" --no-headers)
+
+while IFS= read -r line; do
+    node_name=$(echo "$line" | awk '{print $1}')
+    ip_address=$(echo "$line" | awk '{print $2}')
+    node_ip_mapping["$node_name"]=$ip_address
+done <<< "$node_info"
+
+for node in "${!node_ip_mapping[@]}"; do
+    if [ "$node" == "c1-master" ]; then
+        continue
+    else
+        scp -r ./build root@${node_ip_mapping[$node]}:/home/host-api-service
+        scp "2.add-service.sh" root@${node_ip_mapping[$node]}:/home/host-api-service
+        scp "3.start.sh" root@${node_ip_mapping[$node]}:/home/host-api-service
+    fi
+done
